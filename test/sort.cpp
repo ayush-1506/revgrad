702d2f4cf9a2c8bbd6814289d7635b5f879c7d97
ayush-1506 <ayush.shridhar1506@gmail.com>
2021-10-04 12:13:50 +0530

    sample test

---
+++ test/sort.cpp
@@ -0,0 +1,20 @@
+#include "gtest/gtest.h"
+#include "src/sort.hpp"
+#include "src/data.hpp"
+
+#include <vector>
+
+TEST(TOPOSORT, SORT) {
+	value v(0);
+	std::vector<int> children;
+	for (int i = 1; i <= 10; i++){
+		v = v + i;
+	}
+	for (auto c: v.children){
+		std::cout << c.data << std::endl;
+		children.push_back(c.data);
+	}
+
+	ASSERT_EQ(children[0], 10);
+	ASSERT_EQ(children[1], 45);
+}
